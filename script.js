926eea7ef251cfaf4d928dc84d89b4ce98e56f5a
MZalfred <cutesy-todo0x@icloud.com>
2024-01-07 11:48:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,14 @@ function fetchReport() {
         })
         .catch(error => console.error('Error:', error));
 }
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.log('Service Worker registration failed:', error);
+      });
+  }
+
